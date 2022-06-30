dc35057b8cb8b8fe93c0cb098689d5ad39d94e9d
kayserjhamillex <anibaljhamil@gmail.com>
2022-06-30 18:54:16 -0500

    ahora si

--- src/app/services/variable.service.ts
+++ src/app/services/variable.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GlobalService {
-  public apiUrlGlobal = 'http://fieldsport.herokuapp.com';
+  public apiUrlGlobal = 'https://fieldsport.herokuapp.com';
   constructor() { }
 }
